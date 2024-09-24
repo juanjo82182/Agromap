99c72a301ab873cbef4998970cae874262c6a049
juanjo82182 <juan_sanchez82182@elpoli.edu.co>
2024-09-24 00:15:57 -0500

    Backend de Registro de Usuarios

--- agro-map/src/auth/auth.controller.ts
+++ agro-map/src/auth/auth.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+
+interface UserDTO{
+  nombre: string;
+  email: string;
+  contrasena: string;
+  telefono?: string;
+  direccion: string;
+}
 
 @Controller('auth')
 export class AuthController {
@@ -10,8 +18,13 @@ export class AuthController {
     return 'log-in';
   }
 
-  @Get('sign-up')
-  signUp(){
-    return 'sign-up';
+  @Get('users')
+  getUsers(){
+    return this.authService.getUsers();
+  }
+
+  @Post('sign-up')
+  signUp(@Body() user: UserDTO){
+    return this.authService.signUp(user.nombre,user.email,user.contrasena,user.telefono,user.direccion);
   }
 }
--- agro-map/src/auth/auth.service.ts
+++ agro-map/src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private prismaService: PrismaService){}
+
+    async getUsers(){
+        return await this.prismaService.usuarios.findMany();
+    }
+
+    async signUp(nombre: string, email: string, contrasena: string, telefono?: string, direccion?: string){
+        try {
+            const userFound = await this.prismaService.usuarios.findUnique({
+                where: {
+                    email,
+                },
+            });
+
+            if(userFound) throw new BadRequestException('El usuario ya existe');
+
+            const user= await this.prismaService.usuarios.create({
+                data: {
+                    nombre,
+                    email,
+                    contrasena,
+                    telefono,
+                    direccion,
+                }
+            });
+            return user;
+        } catch (error) {
+            if(error instanceof BadRequestException){
+                throw error;
+            }
+        throw new Error(error)
+        }
+    }
+}
